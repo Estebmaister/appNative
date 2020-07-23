339584cdb881edc5b2a5e7463167f92f2cb1c4e0
Esteban Camargo <estebmaister@gmail.com>
2020-07-23 01:22:22 -0400

    :white_check_mark: Jest Updated

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { color } from "react-native-reanimated";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "#eee" }}>
+        Open up App.js to start working on your app!
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#223",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
